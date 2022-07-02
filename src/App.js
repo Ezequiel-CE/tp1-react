3d274be3537d80ac1e8bb272a71e6d6507a151ca
Ezequiel <ezequielce490@gmail.com>
2022-07-02 16:06:50 -0300

    finished tp1

--- src/App.js
+++ src/App.js
@@ -1,62 +1,43 @@
 import Form from "./components/Form";
-import ListadoPersonas from "./components/ListadoPersonas";
 import { useState } from "react";
 import uniqid from "uniqid";
+import ListadoTareas from "./components/ListadoTareas";
 
 const App = () => {
-  const [personas, setPersonas] = useState([
-    { id: uniqid(), nombre: "Juan Roman", apellido: "Riquelmes", edad: 44 },
-    { id: uniqid(), nombre: "Peter", apellido: "Parker", edad: 22 },
+  const [tareas, setTareas] = useState([
+    { id: uniqid(), nombre: "tarea 1", lista: true },
+    { id: uniqid(), nombre: "tarea 2", lista: false },
   ]);
 
-  const [personaToEdit, setPersonaToEdit] = useState(null);
-
-  const addPersonaHandler = (persona) => {
-    setPersonas((prevState) => [...prevState, persona]);
+  const addTareaHandler = (tarea) => {
+    setTareas((prevState) => [...prevState, tarea]);
   };
 
-  const deletePersonaHandler = (id) => {
-    setPersonas((prevState) =>
-      prevState.filter((persona) => persona.id !== id)
-    );
+  const deleteTareaHandler = (id) => {
+    setTareas((prevState) => prevState.filter((tarea) => tarea.id !== id));
   };
 
-  const startEditModeHandler = (persona) => {
-    setPersonaToEdit(persona);
-  };
-
-  const stopEditModeHandler = () => {
-    setPersonaToEdit(null);
-  };
-
-  const updatePersonaHandler = (id, newPersona) => {
-    setPersonas((prevState) => {
-      return prevState.map((persona) => {
-        if (persona.id === id) {
-          return { id, ...newPersona };
+  const updateTareaHandler = (id) => {
+    setTareas((prevState) => {
+      return prevState.map((tarea) => {
+        if (tarea.id === id) {
+          return { ...tarea, lista: !tarea.lista };
         }
-        return persona;
+        return tarea;
       });
     });
-    setPersonaToEdit(null);
   };
 
   return (
     <div className="min-h-screen bg-cyan-400  ">
       <h1 className="text-center mb-12 font-bold leading-tight text-4xl bg-slate-50 p-6  shadow-lg md:text-5xl text-gray-700  ">
-        Libreta de Personas
+        Lista de tareas
       </h1>
-      <Form
-        addPersona={addPersonaHandler}
-        stopEdit={stopEditModeHandler}
-        personaToEdit={personaToEdit}
-        updatePersona={updatePersonaHandler}
-      />
-      <ListadoPersonas
-        personasData={personas}
-        deletePersona={deletePersonaHandler}
-        startEdit={startEditModeHandler}
-        personaToEdit={personaToEdit}
+      <Form addTarea={addTareaHandler} />
+      <ListadoTareas
+        tareas={tareas}
+        deleteTarea={deleteTareaHandler}
+        updateTarea={updateTareaHandler}
       />
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,63 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import uniqid from "uniqid";
 
-const Form = ({ addPersona, personaToEdit, stopEdit, updatePersona }) => {
-  const [inputs, setInputs] = useState({ nombre: "", apellido: "", edad: "" });
-  const { nombre, apellido, edad } = inputs;
-
-  const cleanInputs = () => {
-    setInputs({ nombre: "", apellido: "", edad: "" });
-  };
-
-  //accion de input
+const Form = ({ addTarea }) => {
+  const [input, setInput] = useState("");
 
   const onChangeHandler = (e) => {
-    const { name, value } = e.target;
-    setInputs((prevState) => {
-      return { ...prevState, [name]: value };
-    });
+    setInput(e.target.value);
   };
 
   //accion de botones
 
-  const addPersonaOnClick = (e) => {
+  const addTareaOnClick = (e) => {
     e.preventDefault();
-    if (nombre === "" || apellido === "" || edad === "") {
-      window.alert("Rellena todo el formulario");
+    if (input === "") {
+      window.alert("Rellena  el formulario");
       return;
     }
-    addPersona({ id: uniqid(), nombre, apellido, edad });
-    cleanInputs();
+    addTarea({ id: uniqid(), nombre: input, lista: false });
+    setInput("");
   };
-
-  const onCancelEditHandler = (e) => {
-    e.preventDefault();
-    stopEdit();
-    cleanInputs();
-  };
-
-  const onAcceptEditHandler = (e) => {
-    e.preventDefault();
-    if (nombre === "" || apellido === "" || edad === "") {
-      window.alert("Formulario no puede estar vacio");
-      return;
-    }
-    updatePersona(personaToEdit.id, inputs);
-    cleanInputs();
-  };
-
-  //cuando entramos en modo de editar cambia los inputs
-
-  useEffect(() => {
-    if (personaToEdit) {
-      console.log("cambia");
-      setInputs({
-        nombre: personaToEdit.nombre,
-        apellido: personaToEdit.apellido,
-        edad: personaToEdit.edad,
-      });
-    }
-  }, [personaToEdit]);
 
   return (
     <form className="bg-slate-50 p-5 rounded-lg flex flex-col gap-3 shadow-lg w-11/12 md:max-w-lg mx-auto ">
@@ -66,7 +27,7 @@ const Form = ({ addPersona, personaToEdit, stopEdit, updatePersona }) => {
           className="block text-gray-700 text-sm font-bold mb-2"
           htmlFor="nombre"
         >
-          Nombre
+          Tarea
         </label>
         <input
           className="shadow  border rounded w-full py-2 px-3 text-gray-700  focus:outline focus:shadow-outline"
@@ -74,68 +35,18 @@ const Form = ({ addPersona, personaToEdit, stopEdit, updatePersona }) => {
           type="text"
           placeholder="Nombre de la persona"
           onChange={onChangeHandler}
-          value={nombre}
+          value={input}
         />
       </div>
-      <div>
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="apellido"
-        >
-          Apellido
-        </label>
-        <input
-          className="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline focus:shadow-outline"
-          name="apellido"
-          type="text"
-          placeholder="Apellido de la persona"
-          onChange={onChangeHandler}
-          value={apellido}
-        />
-      </div>
-      <div>
-        <label
-          className="block text-gray-700 text-sm font-bold mb-2"
-          htmlFor="edad"
-        >
-          Edad
-        </label>
-        <input
-          className="shadow  border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline focus:shadow-outline"
-          name="edad"
-          type="number"
-          placeholder="Edad de la persona"
-          onChange={onChangeHandler}
-          value={edad}
-        />
-      </div>
+
       <div className="flex items-center justify-center py-3 gap-3">
-        {personaToEdit ? (
-          <>
-            <button
-              className="bg-slate-300 hover:bg-slate-500 text-black font-bold py-2 px-4 rounded-full  "
-              type="submit"
-              onClick={onAcceptEditHandler}
-            >
-              Aceptar
-            </button>
-            <button
-              className="bg-slate-300 hover:bg-slate-500 text-black font-bold py-2 px-4 rounded-full  "
-              type="submit"
-              onClick={onCancelEditHandler}
-            >
-              Cancelar
-            </button>
-          </>
-        ) : (
-          <button
-            className="bg-slate-300 hover:bg-slate-500 text-black font-bold py-2 px-4 rounded-full border focus:outline-1 focus:shadow-outline"
-            type="submit"
-            onClick={addPersonaOnClick}
-          >
-            Agregar
-          </button>
-        )}
+        <button
+          className="bg-slate-300 hover:bg-slate-500 text-black font-bold py-2 px-4 rounded-full  "
+          type="submit"
+          onClick={addTareaOnClick}
+        >
+          Agregar
+        </button>
       </div>
     </form>
   );
